f6eb2d36efff815307b282231a56a3afb4fd7bdc
taketake24 <taketakeworker24@gmail.com>
2024-05-21 21:23:24 +0900

    インテンド整理

--- autoTweet/test.php
+++ autoTweet/test.php
@@ -1,10 +1,10 @@
+use Symfony\Component\HttpClient\HttpClient;
+use Abraham\TwitterOAuth\TwitterOAuth;
+
 <?php
 
 require 'vendor/autoload.php';
 require 'config.php';
-use Symfony\Component\HttpClient\HttpClient;
-use Abraham\TwitterOAuth\TwitterOAuth;
-
 
 // 結果をログファイルに書き込む
 $logFile = '/var/www/html/tool/autoTweet/log/log.txt';
@@ -13,60 +13,59 @@ file_put_contents($logFile, $logMessage, FILE_APPEND);
 
 $client = HttpClient::create();
 
-//1から2の数値をランダムで取得
-$randomNumber = rand(1,2);
-//メインプロンプト取得
-$mainPronpt = file_get_contents("/var/www/html/tool/autoTweet/prompWordList/mainPrompt{$randomNumber}.csv");
+// 1から2の数値をランダムで取得
+$randomNumber = rand(1, 2);
+// メインプロンプト取得
+$mainPrompt = file_get_contents("/var/www/html/tool/autoTweet/prompWordList/mainPrompt{$randomNumber}.csv");
 
-//テーマプロンプト取得
+// テスト用のコード
+
+// テーマプロンプト取得
 $jsonData = file_get_contents("/var/www/html/tool/autoTweet/promptThemeList/promptList.json");
 $themePrompts = json_decode($jsonData, true);
-//ランダムに取得
+// ランダムに取得
 $randomPrompt = $themePrompts[array_rand($themePrompts)];
 
-//置換処理
-$mainPronpt = str_replace('--Theme--', $randomPrompt, $mainPronpt);
+// 置換処理
+$mainPrompt = str_replace('--Theme--', $randomPrompt, $mainPrompt);
 
-//実施するプロンプトを出力
-// var_dump($mainPronpt);
+// 実施するプロンプトを出力
+// var_dump($mainPrompt);
 
 $response = $client->request('POST', 'https://api.openai.com/v1/chat/completions', [
-  'headers' => [
-      'Authorization' => 'Bearer ' . $apiKey,
-      'Content-Type' => 'application/json',
-  ],
-  'json' => [
-      'model' => 'gpt-3.5-turbo',
-      'messages' => [
-          [
-              'role' => 'system',
-              'content' => 'You are a helpful assistant.'
-          ],
-          [
-              'role' => 'user',
-              'content' => $mainPronpt
-          ]
-      ],
-      'max_tokens' => 500,
-  ],
+    'headers' => [
+        'Authorization' => 'Bearer ' . $apiKey,
+        'Content-Type' => 'application/json',
+    ],
+    'json' => [
+        'model' => 'gpt-3.5-turbo',
+        'messages' => [
+            [
+                'role' => 'system',
+                'content' => 'You are a helpful assistant.'
+            ],
+            [
+                'role' => 'user',
+                'content' => $mainPrompt
+            ]
+        ],
+        'max_tokens' => 500,
+    ],
 ]);
 
-//結果を格納
+// 結果を格納
 $result = $response->toArray();
 
-
-//promptの文言を出力
+// promptの文言を出力
 $responseOpenAIWord = $result['choices'][0]['message']['content'];
 // $responseOpenAIWord = "";
 var_dump($responseOpenAIWord);
 
-
 // 結果をログファイルに書き込む
 $logFile = '/var/www/html/tool/autoTweet/log/log.txt';
-$text = mb_convert_encoding(json_encode($result),"utf-8","sjis-win");
+$text = mb_convert_encoding(json_encode($result), "utf-8", "sjis-win");
 $logMessage = date('Y-m-d H:i:s') . ': ' . $text . PHP_EOL;
 file_put_contents($logFile, $logMessage, FILE_APPEND);
-
 
 // 文字数を取得
 $wordCount = mb_strlen($responseOpenAIWord);
@@ -98,7 +97,7 @@ while ($wordCount <= 144 && $counter < 20) {
 // TwitterOAuthオブジェクトを作成
 $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 
-//v2使用を宣言
+// v2使用を宣言
 $connection->setApiVersion('2');
 
 // ツイートを送信
@@ -115,4 +114,3 @@ if ($connection->getLastHttpCode() == 200) {
 $logFile = '/var/www/html/tool/autoTweet/log/log.txt';
 $logMessage = date('Y-m-d H:i:s') . ': ' . json_encode($result) . PHP_EOL;
 file_put_contents($logFile, $logMessage, FILE_APPEND);
-
